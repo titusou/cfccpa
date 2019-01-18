5ff5879b1708bd15da236c776fcc8615e585ab68
danielhan2362 <44785131+danielhan2362@users.noreply.github.com>
2019-01-17 19:11:04 -0800

    added landing pages to menu items (#3)

--- src/App.js
+++ src/App.js
@@ -12,49 +12,35 @@ class App extends Component {
         <Navbar>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#home">CFCCPA</a>
+              <a href="http://localhost:3000/">CFCCPA</a>
             </Navbar.Brand>
           </Navbar.Header>
           <Nav>
-            <NavItem eventKey={2} href="#" />
             <NavDropdown eventKey={1} title="About Us" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1} href="/mission/">
-                Mission
-              </MenuItem>
-              <MenuItem eventKey={3.2}>Faith</MenuItem>
-              <MenuItem eventKey={3.3}>Sunday Schedule</MenuItem>
-              <MenuItem eventKey={3.4}>Staff</MenuItem>
+              <MenuItem eventKey={1.1} href="/mission/">Mission</MenuItem>
+              <MenuItem eventKey={1.2} href="/faith/">Faith</MenuItem>
+              <MenuItem eventKey={1.3} href="/sunday-schedule/">Sunday Schedule</MenuItem>
+              <MenuItem eventKey={1.4} href="/staff/">Staff</MenuItem> 
             </NavDropdown>
-            <NavItem eventKey={2} href="#">
-              Contact Us
-            </NavItem>
-            <NavDropdown
-              eventKey={3}
-              title="Church Life"
-              id="basic-nav-dropdown"
-            >
-              <MenuItem eventKey={3.1}>Cell Groups</MenuItem>
-              <MenuItem eventKey={3.2}>Sunday School</MenuItem>
-              <MenuItem eventKey={3.3}>Baptism</MenuItem>
-              <MenuItem eventKey={3.4}>Bible Study</MenuItem>
+            <NavItem eventKey={2} href="/contact-us/">Contact Us</NavItem>
+            <NavDropdown eventKey={3} title="Church Life" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1} href="/fellowship-groups">Fellowship Groups</MenuItem>
+              <MenuItem eventKey={3.3} href="/baptism">Baptism</MenuItem>
+              <MenuItem eventKey={3.4} href="/bible-study">Bible Study</MenuItem>
             </NavDropdown>
             <NavDropdown eventKey={4} title="Message" id="basic-nav-dropdown">
-              <MenuItem eventKey={4.1}>Sunday Sermon</MenuItem>
-              <MenuItem eventKey={4.2}>Sunday School</MenuItem>
+              <MenuItem eventKey={4.1} href="/sunday-sermon">Sunday Sermon</MenuItem>
+              <MenuItem eventKey={4.2} href="/sunday-school">Sunday School</MenuItem>
             </NavDropdown>
-            <NavDropdown
-              eventKey={5}
-              title="Community Service"
-              id="basic-nav-dropdown"
-            >
-              <MenuItem eventKey={5.1}>Citizenship Class</MenuItem>
-              <MenuItem eventKey={5.2}>English Class</MenuItem>
-              <MenuItem eventKey={5.3}>SoulFood</MenuItem>
-              <MenuItem eventKey={5.4}>Chinese Treatment</MenuItem>
+            <NavDropdown eventKey={5} title="Community Service" id="basic-nav-dropdown">
+              <MenuItem eventKey={5.1} href="/citizenship-class">Citizenship Class</MenuItem>
+              <MenuItem eventKey={5.2} href="/english-class">English Class</MenuItem>
+              <MenuItem eventKey={5.3} href="/soul-food">Soul Food</MenuItem>
+              <MenuItem eventKey={5.4} href="/chinese-treatment">Chinese Treatment</MenuItem>
             </NavDropdown>
             <NavDropdown eventKey={6} title="English" id="basic-nav-dropdown">
-              <MenuItem eventKey={6.1}>English</MenuItem>
-              <MenuItem eventKey={6.2}>Chinese</MenuItem>
+              <MenuItem eventKey={6.1} href="/english">English</MenuItem>
+              <MenuItem eventKey={6.2} href="/chinese">Chinese</MenuItem>
             </NavDropdown>
           </Nav>
         </Navbar>
@@ -62,6 +48,21 @@ class App extends Component {
           <div>
             <Route exact path="/" component={Home} />
             <Route path="/mission" render={() => "mission"} />
+            <Route path="/faith" render={() => "faith"} />
+            <Route path="/sunday-schedule" render={() => "sunday schedule"} />
+            <Route path="/staff" render={() => "staff"} />
+            <Route path="/contact-us" render={() => "contact us"} />
+            <Route path="/fellowship-groups" render={() => "fellowship groups"} />
+            <Route path="/baptism" render={() => "baptism"} />
+            <Route path="/bible-study" render={() => "bible study"} />
+            <Route path="/sunday-sermon" render={() => "sunday sermon"} />
+            <Route path="/sunday-school" render={() => "sunday school"} />
+            <Route path="/citizenship-class" render={() => "citizenship class"} />
+            <Route path="/english-class" render={() => "english class"} />
+            <Route path="/soul-food" render={() => "soul food"} />
+            <Route path="/chinese-treatment" render={() => "chinese treatment"} />
+            <Route path="/english" render={() => "english"} />
+            <Route path="/chinese" render={() => "chinese"} />
           </div>
         </Router>
       </div>
