b4f0ac38ceddb4d4cccb7eea6b4d1c3647dfe2bb
danielhan2362 <44785131+danielhan2362@users.noreply.github.com>
2019-03-19 11:25:32 -0700

    Menu items expand when hovering. Added Sunday School component. (#12)
    * Menu items expand when hovering. Added Sunday School component.
    * removed console.logs
    * made changes to pass onToggle handler when we override open

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,48 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class ContactUs extends Component {
-  render() {
-    return (
-      <div className="contact-items">
-        <div
-          className="contact-box"
-          onClick={() => {
-            window.open("https://www.facebook.com/cfccpa/");
-          }}
-        >
-          <i className="fa fa-facebook-square fa-5x contact-icon-facebook" />
-          <div className="logo-text">Facebook</div>
-          <div>
-            https://www.facebook.com/cfccpa/
-          </div>
-        </div>
-        <div
-          className="contact-box"
-          onClick={() =>
-            (window.location.href = "mailto:cfccpa@cfccpaloalto.org")
-          }
-        >
-          <i className="fa fa-envelope-square fa-5x contact-icon-mailto" />
-          <div className="logo-text">Email</div>
-          <div>
-            cfccpa@cfccpaloalto.org
-          </div>
-        </div>
-        <div
-          className="contact-box"
-          onClick={() => 
-            (window.location.href = "tel:+6505464215")
-          }
-        >
-          <i className="fa fa-phone-square fa-5x contact-icon-tel" />
-          <div className="logo-text">Phone</div>
-          <div>
-            (650) 546-4215
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="contact-items">
+                <div
+                    className="contact-box"
+                    onClick={() => {
+                        window.open('https://www.facebook.com/cfccpa/');
+                    }}
+                >
+                    <i className="fa fa-facebook-square fa-5x contact-icon-facebook" />
+                    <div className="logo-text">Facebook</div>
+                    <div>https://www.facebook.com/cfccpa/</div>
+                </div>
+                <div className="contact-box" onClick={() => (window.location.href = 'mailto:cfccpa@cfccpaloalto.org')}>
+                    <i className="fa fa-envelope-square fa-5x contact-icon-mailto" />
+                    <div className="logo-text">Email</div>
+                    <div>cfccpa@cfccpaloalto.org</div>
+                </div>
+                <div className="contact-box" onClick={() => (window.location.href = 'tel:+6505464215')}>
+                    <i className="fa fa-phone-square fa-5x contact-icon-tel" />
+                    <div className="logo-text">Phone</div>
+                    <div>(650) 546-4215</div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default ContactUs;
---
+++ src/components/SundaySchool.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SundaySchool extends Component {
+    render() {
+        return (
+            <div>
+                <div className="sunday-school-title-block">
+                    <div className="sunday-school-title">Sunday School</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SundaySchool;
