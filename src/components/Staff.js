04a58424c416968b092470c5484d529fa1614115
titusou <ou.titus@gmail.com>
2019-03-24 10:37:58 -0700

    staff page updates (#15)

--- src/components/Staff.js
+++ src/components/Staff.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
 import { Email, Call } from '@material-ui/icons';
 
-import johnZhou from '../assets/john-zhou.jpg';
 import steveCheng from '../assets/steve-cheng.jpg';
 
 class Staff extends Component {
@@ -38,13 +37,12 @@ class Staff extends Component {
                 </div>
                 <div className="staff-cards">
                     {this.renderCard(
-                        'Pastor Steve Cheng',
+                        'Elder Steve Cheng',
                         steveCheng,
                         'yichunsteve@cfccpaloalto.org',
                         '4087817028',
                         `Steve came to Christ during his studies in the United States. Upon graduation, Steve started a computer parts company in Silicon Valley. After running it for more than ten years, Steve made the decision to enter into full-time ministry. In 2015, Steve graduated from Overseas Theological Seminary. Steve and his wife, On Lee, are blessed with a son and two daughters.`
                     )}
-                    {this.renderCard('Minister John Zhou', johnZhou)}
                 </div>
             </div>
         );
