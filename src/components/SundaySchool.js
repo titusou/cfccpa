19be78ed7629788dd1d5d805ece7f5b1bfbee362
Titus Ou <ou.titus@gmail.com>
2019-08-26 00:44:11 -0700

    fall 2019 flyers

--- src/components/SundaySchool.js
+++ src/components/SundaySchool.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react';
 import { Paper } from '@material-ui/core';
 
 import sundaySchool from '../assets/sunday-school.png';
+import sundaySchoolChildren from '../assets/sunday-school-children.png';
+
+import './sundayschool.css';
 
 class SundaySchool extends Component {
     render() {
+        const flyers = [
+            { name: 'Sunday School', src: sundaySchool },
+            { name: `Sunday School for Children`, src: sundaySchoolChildren },
+        ];
+
         return (
             <div>
-                <div className="sunday-school-title-block">
-                    <div className="sunday-school-title">Sunday School</div>
+                <div className="page-title-block page-title-block-bg-default">
+                    <div className="page-title">Sunday School</div>
                 </div>
-                <div className="sunday-school-flyer">
-                    <div className="home-section">
-                        <Paper className="home-theme-container" square>
-                            <img alt="Sunday School" className="home-sunday-school-img" src={sundaySchool} />
-                        </Paper>
-                    </div>
+                <div className="sunday-school-flyers">
+                    {flyers.map((flyer, idx) => {
+                        const { name, src } = flyer || {};
+
+                        return (
+                            <Paper className="sunday-school-img-container" key={idx} square>
+                                <img alt={name} className="sunday-school-img" src={src} />
+                            </Paper>
+                        );
+                    })}
                 </div>
             </div>
         );
