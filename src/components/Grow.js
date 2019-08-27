4b5e60e802ef7220d984cac5c9f7f69cc6b3546b
Titus Ou <ou.titus@gmail.com>
2019-08-26 22:37:46 -0700

    sisters fellowship

--- src/components/Grow.js
+++ src/components/Grow.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import { Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
 import { Email } from '@material-ui/icons';
 
+import fellowshipSisters from '../assets/fellowship-sisters.png';
+
+import './grow.css';
+
 class Grow extends Component {
     renderCard(name, description, contacts = [], image) {
         return (
-            <Card className="grow-card">
-                {image ? <CardMedia className="grow-card-img" component="img" image={image} /> : null}
+            <Card className="cfccpa-card">
+                {image ? <CardMedia className="cfccpa-card-img" component="img" image={image} /> : null}
                 <CardContent>
-                    <div className="grow-card-name">{name}</div>
-                    <div className="grow-card-description">{description}</div>
+                    <div className="cfccpa-card-name">{name}</div>
+                    <div className="cfccpa-card-description">{description}</div>
                 </CardContent>
                 {contacts.length > 0 ? (
-                    <CardActions className="grow-card-actions" disableActionSpacing>
+                    <CardActions className="cfccpa-card-actions" disableActionSpacing>
                         {contacts.map(contact => {
                             const { email, name } = contact || {};
                             return (
@@ -30,7 +34,7 @@ class Grow extends Component {
     render() {
         return (
             <div className="grow">
-                <div className="grow-header">
+                <div className="grow-header page-title-block-bg-default">
                     <div>
                         <div className="grow-header-text-lg">GROW</div>
                         <div className="grow-header-text-sm">
@@ -45,14 +49,10 @@ class Grow extends Component {
                     </div>
                 </div>
                 <div className="grow-title">Our Fellowship Groups</div>
-                <div className="grow-cards">
-                    {this.renderCard('Vine', 'Vine meets on the 2nd and 4th Fridays of the month at 8:00 PM.', [
-                        { email: 'julie_tai_99@yahoo.com', name: 'Julie Tai' },
-                        { email: 'chang162@aol.com ', name: 'Augustin Chang' },
-                    ])}
-                    {this.renderCard('Elim', 'Elim meets on the 1st and 3rd Saturdays of the month at 7:00 PM.', [
-                        { email: 'yaolihua081@gmail.com', name: 'Li Hua Yao' },
-                    ])}
+                <div className="cfccpa-cards">
+                    <Card className="cfccpa-card">
+                        <CardMedia className="cfccpa-card-img" component="img" image={fellowshipSisters} />
+                    </Card>
                     {this.renderCard(
                         'Manna',
                         'A fellowship group for young adults. We meet every other Sunday at 1:30 PM.',
@@ -61,9 +61,16 @@ class Grow extends Component {
                             { email: 'rjchow@gmail.com', name: 'Rosie Chow' },
                         ]
                     )}
+                    {this.renderCard('Vine', 'Vine meets on the 2nd and 4th Fridays of the month at 8:00 PM.', [
+                        { email: 'julie_tai_99@yahoo.com', name: 'Julie Tai' },
+                        { email: 'chang162@aol.com ', name: 'Augustin Chang' },
+                    ])}
+                    {this.renderCard('Elim', 'Elim meets on the 1st and 3rd Saturdays of the month at 7:00 PM.', [
+                        { email: 'yaolihua081@gmail.com', name: 'Li Hua Yao' },
+                    ])}
                 </div>
                 <div className="grow-title">Join a Bible Study Group</div>
-                <div className="grow-cards">
+                <div className="bible-study cfccpa-cards">
                     {this.renderCard(
                         'Morning Bible Reading',
                         'We get together on a regular basis at 7:00 AM bright and early in the morning to read and discuss passages as we read through the Bible together. Join us in our WeChat group at (831) 392-6627.'
